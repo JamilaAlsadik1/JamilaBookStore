a1d7bcd670331a908fd92223954c70c25e80cc4e
Jamila Alsadik <W0759725@myscc.ca>
2021-11-15 10:50:04 -0500

    adding IRepositary interface

---
+++ JamilaBooks.DataAccess/Repositary/IRepositary/IRepositary.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JamilaBooks.DataAccess.Repositary.IRepositary
+{
+    public interface IRepository<T> where T : class
+    {
+        T Get(int id);                  // Retrieve a category from the database by id 
+        //List of Categories based on requirements 
+
+        IEnumerable<T> GetAll(
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            string includeProperties = null             // useful  for foreign key references
+            );
+
+       
+    }
+}
