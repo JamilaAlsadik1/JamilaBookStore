7fb98c150d1bd972035f4d0b1b88758d25e1bde8
Jamila Alsadik <W0759725@myscc.ca>
2021-11-15 12:22:54 -0500

    Adding ISP_Call.cs and unitkOfWork interface and class

---
+++ JamilaBooks.DataAccess/Repositary/IRepositary/UnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JamilaBooks.DataAccess.Repositary.IRepositary
+{
+    interface UnitOfWork:IDisposable
+    {
+        ICategoryRepositary Category { get; }
+        ISP_Call SP_Call { get; }
+    }
+}
---
+++ JamilaBooks.DataAccess/Repositary/UnitOfWork.cs
@@ -0,0 +1,33 @@
+﻿using JamilaBooks.DataAccess.Repositary.IRepositary;
+using JamilaBooks.Models;
+using JamilaBookStore.DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JamilaBooks.DataAccess.Repositary
+{
+    class UnitOfWork
+    {
+        private readonly ApplicationDbContext _db;
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            Category = new CategoryRepositary(_db);
+            SP_Call= new SP_Call(_db);
+
+        }
+        public CategoryRepositary Category { get; private set; }
+        public ISP_Call SP_Call { get; private set; }
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
