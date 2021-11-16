4aa6fe9ff5a31733e311ec54b80a931e9067d60c
JamilaAlsadik1 <w0759725@myscc.ca>
2021-11-16 10:01:39 -0500

    adding comment to IcategoryRepository.cs because of adding method to correct error in categorycontroller.cs

--- JamilaBooks.DataAccess/Repository/IRepository/ICategoryRepository.cs
+++ JamilaBooks.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -10,6 +10,6 @@ namespace JamilaBooks.DataAccess.Repository.IRepository
    public interface ICategoryRepository:IRepository<Category>
     {
         void Ubdate(Category category);
-        void Remove(Category objFromDb);
+        void Remove(Category objFromDb);//I don't know if it's correct to add it
     }
 }
