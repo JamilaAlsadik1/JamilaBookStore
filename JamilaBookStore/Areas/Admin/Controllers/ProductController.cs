34d426f09b8ebec7b11a626cc0ae647f4ac7a923
Jamila Alsadik <W0759725@myscc.ca>
2021-11-24 10:34:13 -0500

    update productController and adding upsert method with some modification

--- JamilaBookStore/Areas/Admin/Controllers/ProductController.cs
+++ JamilaBookStore/Areas/Admin/Controllers/ProductController.cs
@@ -60,7 +60,26 @@ namespace JamilaBookStore.Areas.Admin.Controllers
             }
             return View(productVM);
         }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                if (product.Id == 0)
+                {
+                    _unitOfWork.Product.Add(product);
+                }
+                else
+                {
+                    _unitOfWork.Product.Update(product);
+                }
+                _unitOfWork.Save();
+                return RedirectToAction(nameof(Index)); //To see all categories
 
+            }
+            return View(product);
+        }
         #region API CALLS
         [HttpGet]
 
